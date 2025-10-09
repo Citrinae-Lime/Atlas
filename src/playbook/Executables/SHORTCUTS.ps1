35abc67a2e8a05ebe24e44e97eb9801f39298b5a
Citrinae-Lime <Citrinae-Lime@users.noreply.github.com>
2025-10-09 22:21:03 +0200

    修改 tweaks 与其对应项

--- src/playbook/Executables/SHORTCUTS.ps1
+++ src/playbook/Executables/SHORTCUTS.ps1
@@ -4,20 +4,20 @@ $windir = [Environment]::GetFolderPath('Windows')
 Write-Title "Creating Desktop & Start Menu shortcuts..."
 
 # Default user
-$defaultShortcut = "$(Get-UserPath)\Atlas.lnk"
-New-Shortcut -Source "$windir\AtlasDesktop" -Destination $defaultShortcut -Icon "$windir\AtlasModules\Other\atlas-folder.ico,0"
+# $defaultShortcut = "$(Get-UserPath)\Atlas.lnk"
+# New-Shortcut -Source "$windir\AtlasDesktop" -Destination $defaultShortcut -Icon "$windir\AtlasModules\Other\atlas-folder.ico,0"
 
 # Copy shortcut to every user
-foreach ($userKey in (Get-RegUserPaths -NoDefault).PsPath) {
-	$folders = Get-ItemProperty -path "$userKey\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders"
-	$deskPath = $folders.Desktop
-	if (Test-Path $deskPath -PathType Container) {
-		Write-Output "Copying Desktop shortcut for '$userKey'..."
-		Copy-Item $defaultShortcut -Destination $deskPath -Force
-	} else {
-		Write-Error "Desktop path not found for '$userKey', shortcuts can't be copied."
-	}
-}
+# foreach ($userKey in (Get-RegUserPaths -NoDefault).PsPath) {
+# 	$folders = Get-ItemProperty -path "$userKey\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders"
+# 	$deskPath = $folders.Desktop
+# 	if (Test-Path $deskPath -PathType Container) {
+# 		Write-Output "Copying Desktop shortcut for '$userKey'..."
+# 		Copy-Item $defaultShortcut -Destination $deskPath -Force
+# 	} else {
+# 		Write-Error "Desktop path not found for '$userKey', shortcuts can't be copied."
+# 	}
+# }
 
 # Start menu shortcut
 Copy-Item $defaultShortcut -Destination "$([Environment]::GetFolderPath('CommonStartMenu'))\Programs" -Force
